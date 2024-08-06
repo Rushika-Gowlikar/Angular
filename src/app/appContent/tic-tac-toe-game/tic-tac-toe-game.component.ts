dbc99c6957b6b0489bf944f0bd7514a5839bbd46
Rushika-Gowlikar <rushikagowlikar@gmail.com>
2024-08-06 21:13:29 +0530

    game changes

--- src/app/appContent/tic-tac-toe-game/tic-tac-toe-game.component.ts
+++ src/app/appContent/tic-tac-toe-game/tic-tac-toe-game.component.ts
@@ -6,18 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tic-tac-toe-game.component.css'],
 })
 export class TicTacToeGameComponent implements OnInit {
+  reformat: string[];
+  onAndoff: boolean = true;
+  updated: any;
+  isDisabled: boolean = false;
+
   constructor() {}
   playerDetails: any;
   isStart: boolean = false;
   playerO: any;
   playerX: any;
   text = 'Submit Player Names To Start Game!';
-  boxes = [null, null, null, 'X', null, null, null, 'O', null];
+  boxes = ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'];
   ngOnInit(): void {}
   onSubmit(details) {
     this.isStart = true;
     this.text = this.isStart
       ? 'let Start'
       : 'Submit Player Names To Start Game!';
+
+    this.playerX = details.form.get('name-x').value;
+    this.playerO = details.form.get('name-o').value;
+  }
+  squareChecked(i, box) {
+    if (box == 'P') {
+      this.onAndoff = !this.onAndoff;
+      this.boxes.map((value, key) => {
+        if (key == i) {
+          value = this.onAndoff ? 'X' : 'O';
+          this.boxes.splice(i, 1, value);
+        }
+      });
+    }
   }
 }
